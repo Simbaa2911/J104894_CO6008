01a92ecf369b8a4a734868e73257f30267f31ca1
JehanM1 <5442@laksamanacollege.edu.bn>
2025-06-11 12:23:23 +0800

    Deploy

--- start.sh
+++ start.sh
@@ -8,5 +8,6 @@ echo "PORT inside container = $PORT"
 envsubst '$PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # launch components
-uvicorn backend.app:app --host 127.0.0.1 --port 8000 &
+#uvicorn backend.app:app --host 127.0.0.1 --port 8000 &
+uvicorn backend.app:app --host 127.0.0.1 --port 8000 \ --log-level debug &
 exec nginx -g 'daemon off;'
